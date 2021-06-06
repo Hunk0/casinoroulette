55be2b133d15628dfc0c7ebd9db862625ffa59ea
Hunk0 <hricaurtec@correo.udistrital.edu.co>
2021-06-06 17:37:21 -0500

    enpoinds implemented

--- src/main/java/com/clean/casinoroulette/controller/RouletteController.java
+++ src/main/java/com/clean/casinoroulette/controller/RouletteController.java
@@ -1,28 +1,87 @@
 package com.clean.casinoroulette.controller;
 
+import com.clean.casinoroulette.model.Bet;
 import com.clean.casinoroulette.model.Roulette;
 import com.clean.casinoroulette.repository.BetRepository;
 import com.clean.casinoroulette.repository.RouletteRepository;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.context.config.ConfigDataResourceNotFoundException;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
-
+import org.springframework.web.bind.annotation.*;
 import java.util.List;
+import java.util.Optional;
+import java.util.concurrent.ThreadLocalRandom;
 
 @RestController
 @RequestMapping("/roulettes")
 public class RouletteController {
     @Autowired
     RouletteRepository rouletteRepository;
-
     @Autowired
     BetRepository betReRepository;
 
-    @GetMapping()
-    public ResponseEntity<String> testFunction() {
-        return new ResponseEntity<>("It works!", HttpStatus.OK);
+    @GetMapping("")
+    public List<Roulette> getAllRoulettes() {
+        return rouletteRepository.findAll();
+    }
+
+    @PostMapping("/newroulette")
+    public ResponseEntity<Object> createRoulette() {
+        return new ResponseEntity<>(rouletteRepository.save(new Roulette()), HttpStatus.CREATED);
+    }
+
+    @PostMapping("/{id}/bet")
+    public ResponseEntity<Object> doBet(@RequestHeader(value = "userId") Long userId, @PathVariable(value = "id") Long id, @RequestBody Bet bet) throws ConfigDataResourceNotFoundException {
+        Optional<Roulette> rouletteOptional = Optional.ofNullable(rouletteRepository.getById(id));
+        if(rouletteOptional.isPresent()){
+            if(rouletteOptional.get().getOpen()){
+                Boolean numberBet = !bet.getValue().replaceAll("[^\\d]", "").equals("");
+                bet.setRoulette(rouletteOptional.get());
+                bet.setUserId(userId);
+                if((numberBet) ? (Integer.parseInt(bet.getValue()) < 0 || Integer.parseInt(bet.getValue()) > 36) : !(bet.getValue().equals("negro") || bet.getValue().equals("rojo")))
+                    return ResponseEntity.status(400).contentType(MediaType.APPLICATION_JSON).body("{\"message\": \"Esta apuesta no es valida\"}");
+
+                return new ResponseEntity<>(betReRepository.save(bet), HttpStatus.CREATED);
+            }
+
+            return ResponseEntity.badRequest().contentType(MediaType.APPLICATION_JSON).body("{\"message\": \"Esta ruleta no acepta apuestas actualmente\"}");
+        }
+
+        return ResponseEntity.notFound().build();
+    }
+
+    @PutMapping("/{id}/open")
+    public ResponseEntity<Object> openRoulette(@PathVariable(value = "id") Long id) throws ConfigDataResourceNotFoundException {
+        Optional<Roulette> rouletteOptional = Optional.ofNullable(rouletteRepository.getById(id));
+        if(rouletteOptional.isPresent()){
+            if(rouletteOptional.get().getOpen()) return ResponseEntity.status(409).contentType(MediaType.APPLICATION_JSON).body("{\"message\": \"Esta ruleta ya se encuentra abierta\"}");
+            rouletteOptional.get().setOpen(true);
+            rouletteRepository.save(rouletteOptional.get());
+
+            return ResponseEntity.ok().contentType(MediaType.APPLICATION_JSON).body("{\"message\": \"La ruleta esta abierta, hagan sus apuestas!\"}");
+        }
+
+        return ResponseEntity.notFound().build();
+    }
+
+    @PutMapping("/{id}/close")
+    public ResponseEntity<Object> closeRoulette(@PathVariable(value = "id") Long id) throws ConfigDataResourceNotFoundException, JsonProcessingException {
+        Optional<Roulette> rouletteOptional = rouletteRepository.findById(id);
+        if(rouletteOptional.isPresent()){
+            if(!rouletteOptional.get().getOpen()) return ResponseEntity.status(409).contentType(MediaType.APPLICATION_JSON).body("{\"message\": \"Esta ruleta ya se encuentra cerrada\"}");
+            rouletteOptional.get().setOpen(false);
+            rouletteRepository.save(rouletteOptional.get());
+
+            return ResponseEntity
+                    .ok().contentType(MediaType.APPLICATION_JSON)
+                    .body("{\"message\": " + "\"La ruleta se ha cerrado... " + rouletteOptional.get().spinRoulette() + "\","+
+                            "\"results\": " + new ObjectMapper().writeValueAsString(rouletteOptional.get().getBets()) + "}");
+        }
+
+        return ResponseEntity.notFound().build();
     }
 }
--- src/main/java/com/clean/casinoroulette/model/Roulette.java
+++ src/main/java/com/clean/casinoroulette/model/Roulette.java
@@ -2,6 +2,7 @@ package com.clean.casinoroulette.model;
 
 import javax.persistence.*;
 import java.util.List;
+import java.util.concurrent.ThreadLocalRandom;
 
 @Entity
 @Table(name = "roulette")
@@ -19,28 +20,39 @@ public class Roulette {
         this.open = false;
         this.bets = List.of();
     }
-
     public Long getRouletteId() {
         return rouletteId;
     }
-
     public void setRouletteId(Long rouletteId) {
         this.rouletteId = rouletteId;
     }
-
     public Boolean getOpen() {
         return open;
     }
-
     public void setOpen(Boolean open) {
         this.open = open;
     }
-
     public List<Bet> getBets() {
         return bets;
     }
-
     public void setBets(List<Bet> bets) {
         this.bets = bets;
+    }
+
+    public String spinRoulette() {
+        int winnerNumber = ThreadLocalRandom.current().nextInt(0, 37);
+        String winnerColor = (winnerNumber % 2 == 0) ? "rojo" : "negro";
+        for(Bet bet: this.bets) {
+            Boolean numberBet = !bet.getValue().replaceAll("[^\\d]", "").equals("");
+            if(numberBet && Integer.parseInt(bet.getValue()) == winnerNumber){
+                bet.setCash(bet.getCash() * 5);
+            }else if(bet.getValue().equals(winnerColor)){
+                bet.setCash(bet.getCash() * 1.8);
+            }else{
+                bet.setCash(0.0);
+            }
+        }
+
+        return "El numero ganador ha sido el " + winnerNumber + "(color " + winnerColor + "), gracias por participar!";
     }
 }
