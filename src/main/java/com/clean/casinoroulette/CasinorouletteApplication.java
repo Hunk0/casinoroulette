84bf94013aa29f4ce7026941881e7eeeae95a666
Hunk0 <hricaurtec@correo.udistrital.edu.co>
2021-06-06 16:02:23 -0500

    first commit

---
+++ src/main/java/com/clean/casinoroulette/CasinorouletteApplication.java
@@ -0,0 +1,13 @@
+package com.clean.casinoroulette;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CasinorouletteApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CasinorouletteApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/clean/casinoroulette/CasinorouletteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.clean.casinoroulette;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CasinorouletteApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
